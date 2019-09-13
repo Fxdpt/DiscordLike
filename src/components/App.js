4ce46e475530a9e078385cde95d3eedf7d629a81
Fxdpt <jaouen.jeremypro@gmail.com>
2019-09-13 08:33:59 +0200

    start setting up FAwesome lib

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
-import AtomsCheatSheet from "./atoms/AtomsCheatSheet";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
+import AtomsCheatSheet from "./atoms/AtomsCheatSheet"
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+library.add(fab, faAngleDown)
+
+
 
 function App() {
   return (
     <div className="App">
-      <AtomsCheatSheet></AtomsCheatSheet>
+      <AtomsCheatSheet />
     </div>
   );
 }
